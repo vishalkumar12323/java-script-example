ab39179af375c150e1dd5f0278d1c992542d86ea
Vishal Kumar <vishalninaniya8949info@gmail.com>
2025-02-05 16:13:24 +0530

    added components

---
+++ src/app/components/index.ts
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import MainMenu from "./side-bar/Menu";
+import ListView from "./side-bar/List-view";
+import LoadingButton from "./Loading-button";
+import Button from "./Button";
+
+export { Navbar, Footer, MainMenu, ListView, Button, LoadingButton };
